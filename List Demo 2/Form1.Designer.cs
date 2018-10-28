5cee498f7cb706e0e9d353fad60cab70c5fb8a2d
Eric Mailman <emailman@dtcc.edu>
2018-10-28 08:34:29 -0400

    Initial version

--- List Demo 2/Form1.Designer.cs
+++ List Demo 2/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lbxNewCars = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbxNewCars
+            // 
+            this.lbxNewCars.FormattingEnabled = true;
+            this.lbxNewCars.Location = new System.Drawing.Point(75, 51);
+            this.lbxNewCars.Name = "lbxNewCars";
+            this.lbxNewCars.Size = new System.Drawing.Size(120, 95);
+            this.lbxNewCars.TabIndex = 0;
+            this.lbxNewCars.SelectedIndexChanged += new System.EventHandler(this.lbxNewCars_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(72, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "New Cars";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbxNewCars);
+            this.Name = "Form1";
+            this.Text = "Honda Land Auto";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lbxNewCars;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- List Demo 2/Form1.cs
+++ List Demo 2/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace List_Demo_2
@@ -15,6 +9,51 @@ namespace List_Demo_2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Load an inventory of cars into the list box
+            var inventory = new List<Car>
+            {
+                new Car("Fit", 4, "Red", "LX"),
+                new Car("Civic", 4, "Blue", "EX"),
+                new Car("Civic", 4, "Blue", "EX")
+            };
+
+            lbxNewCars.Items.AddRange(inventory.ToArray());
+        }
+
+        private void lbxNewCars_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Has an item been selected from the list?
+            if (lbxNewCars.SelectedIndex != -1)
+                // Provide an OK-Cancel dialog to complete the sale
+                if (MessageBox.Show($"Selling {lbxNewCars.SelectedItem}?", "Honda Land Auto",
+                    MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                    // If OK selected, remove the item from the inventory
+                    lbxNewCars.Items.RemoveAt(lbxNewCars.SelectedIndex);
+        }
+    }
+
+    class Car
+    {
+        readonly string model;
+        readonly int cylinders;
+        readonly string color;
+        readonly string trim;
+
+        public Car(string _model, int _cylinders, string _color, string _trim)
+        {
+            model = _model;
+            cylinders = _cylinders;
+            color = _color;
+            trim = _trim;
+        }
+
+        public override string ToString()
+        {
+            return $"{model} {trim}\t{cylinders} cyl  {color} ";
         }
     }
 }
